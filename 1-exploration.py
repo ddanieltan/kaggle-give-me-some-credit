35d38439871f926e366691a1755cb88cbb283479
Daniel Tan <danieltan.singapore@gmail.com>
2020-04-17 16:26:47 +0800

    Starting on feature engineering

---
+++ 1-exploration.py
@@ -0,0 +1,70 @@
+#%%
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Plot styling
+sns.set_style("whitegrid")
+sns.set(font_scale=1.5)
+
+# %% Reading data
+train=pd.read_csv('./data/cs-training.csv',index_col=0)
+test=pd.read_csv('./data/cs-test.csv',index_col=0)
+
+# %% Checking quality of data
+train.info()
+# Data types match data definition
+# MonthlyIncome and NumberOfDependents have null values
+
+# %% Checking for missing values
+train.isna().sum()
+print(f'Proportion of null MonthlyIncome= {29731/150000}')
+print(f'Proportion of null NumberOfDependents= {3924/150000}')
+# 20% missing MonthlyIncome slightly concerning
+# 3% missing NumberOfDependents less so
+# leave missing for now, potentially fill in later
+
+
+#%% Checking if the rows with null data contain high predictive power
+train['MonthlyIncome_null']=pd.isnull(train.MonthlyIncome)
+train['NumberOfDependents_null']=pd.isnull(train.NumberOfDependents)
+print(train.groupby('MonthlyIncome_null')['SeriousDlqin2yrs'].mean())
+print(train.groupby('NumberOfDependents_null')['SeriousDlqin2yrs'].mean())
+
+# MonthlyIncome and NumberOfDependents have lower mean SeriousDlqin2yrs
+# for their null vs non-null groups
+# Suggesting predictive power to identify class 1 is low
+# We can take a simple approach to fill in missing values (mean/median/mode)
+
+# %% Checking for target class imbalance
+ax = sns.countplot(x = train.SeriousDlqin2yrs)
+
+target=train.SeriousDlqin2yrs.value_counts()
+for i in target.index:
+    print(f'Class {i}, n={target.at[i]}, proportion={target.at[i]/target.sum()}')
+
+# Class 0 aka No SeriousDlqin2 years 
+# makes up 93% of the dataset
+# severe imbalance
+# We'll have to stratify our sample to account for this imbalance
+
+# %% Feature Engineering
+
+#%% What's the baseline correlation amongst features prior to engineering?
+def plot_corr_heatmap(df):
+    corr=df.corr()
+    mask = np.triu(np.ones_like(corr, dtype=np.bool))   
+    plt.subplots(figsize=(20,15))
+    sns.set(font_scale=5)
+    chart=sns.heatmap(train.corr(),cmap="PiYG",mask=mask)
+    chart.set_xticklabels([])
+    return chart
+
+plot_corr_heatmap(train)
+# As a baseline, most of features show weak correlation with our target
+
+
+
+# %% Check features again
+
