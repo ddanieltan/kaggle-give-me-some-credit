aeb806262b23ca308d3d4574f365fb9261b5e9e7
Daniel Tan <danieltan.singapore@gmail.com>
2020-04-20 14:36:48 +0800

    Finalized pipeline and wrap up

--- main.py
+++ main.py
@@ -9,75 +9,106 @@ from sklearn.model_selection import RandomizedSearchCV
 
 #%% Globals
 RANDOM_STATE = 2020
+RANDOM_PREFIX=np.random.randint(1000,9999)
 
-#%% 
-train=pd.read_csv('./data/cs-training.csv',index_col=0)
-X=train.drop('SeriousDlqin2yrs',axis=1)
-y=train.SeriousDlqin2yrs
+def main():
+    #%% Reading data
+    train=pd.read_csv('./data/cs-training.csv',index_col=0)
+    X=train.drop('SeriousDlqin2yrs',axis=1)
+    y=train.SeriousDlqin2yrs
 
-#Feature Engineering
-
-
-X=(X
-    .pipe(utils.replace_w_sensible_values)
-    .pipe(utils.replace_na)
-    .pipe(utils.log_transform_df)
-    .pipe(utils.add_AgeDecade)
-    # .pipe(utils.add_DebtRatioBin)
-    .pipe(utils.add_features_per_dependent)
-    .pipe(utils.add_features_distance_from_mean)
-    .pipe(utils.add_features_distance_from_median)
-    .pipe(utils.add_features_distance_from_std)
-)
-
-# %%
-X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
-    random_state=RANDOM_STATE,
-    stratify=y
-)
-
-# %%
-lgb_model=lgb.LGBMClassifier(
-    silent=False, 
-    random_state=RANDOM_STATE, 
-    objective='binary',
-    metrics ='auc',
-    boosting='gbdt',
-    scale_pos_weight=14
-)
-
-# %%
-skf=StratifiedKFold(
-    n_splits=10,
-    random_state=RANDOM_STATE
-)
-
-# %%
-tuning_params = {
-    'num_leaves':[5,10,15,31,40,50],
-    'scale_pos_weight':[1,10,14,16], # T/P-1 = 13.96
-    'n_estimators':[100,250,500,750,1000],
-    'learning_rate':[0.025,0.01,0.05,0.1],
-    'max_depth':[3,5,7]
-}
-gs=RandomizedSearchCV(
-    estimator=lgb_model, 
-    param_distributions=tuning_params, 
-    n_iter=50,
-    scoring='roc_auc',
-    cv=skf,
-    refit=True,
-    verbose=True)
-
-gs.fit(X_train,y_train)
-
-#%%
-# Best score reached: 0.8651849275748571 with params: {'scale_pos_weight': 14, 'num_leaves': 10, 'n_estimators': 1000, 'max_depth': 5, 'learning_rate': 0.01} 
-
-# %% Submission
-utils.create_submission(gs)
+    #Feature Engineering
+    print(f'Starting shape: {X.shape}')
+    X=(X
+        .pipe(utils.replace_w_sensible_values)
+        .pipe(utils.replace_na)
+        .pipe(utils.log_transform_df)
+        .pipe(utils.add_AgeDecade)
+        .pipe(utils.add_boolean_DebtRatio_33)
+        .pipe(utils.add_boolean_DebtRatio_43)
+        .pipe(utils.add_features_per_dependent)
+        .pipe(utils.add_features_per_creditline)
+        .pipe(utils.add_features_per_estate)
+        .pipe(utils.add_features_distance_from_mean)
+        .pipe(utils.add_features_distance_from_median)
+        .pipe(utils.add_features_distance_from_std)
+    )
+    print(f'Post feature engineering shape:{X.shape}')
 
 
-# %%
+    # %%
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        random_state=RANDOM_STATE,
+        stratify=y
+    )
+
+    # %% 
+    lgb_model=lgb.LGBMClassifier(
+        silent=False, 
+        random_state=RANDOM_STATE, 
+        objective='binary',
+        metrics ='auc',
+        boosting='gbdt',
+        scale_pos_weight=13.960106382978724 #T/P-1
+    )
+
+    # %%
+    skf=StratifiedKFold(
+        n_splits=5,
+        random_state=RANDOM_STATE
+    )
+
+    # %%
+    tuning_params = {
+        'num_leaves':[5,10,15,31,40,50],
+        'scale_pos_weight':[1,10,14,16], # T/P-1 = 13.96
+        'n_estimators':[100,250,500,750,1000],
+        'learning_rate':[0.025,0.01,0.05,0.1],
+        'max_depth':[3,5,7],
+        'min_child_weight':range(3,6,1),
+        'subsample':[i/10.0 for i in range(6,10)],
+        'colsample_bytree':[i/10.0 for i in range(6,10)],
+    }
+
+    gs=RandomizedSearchCV(
+        estimator=lgb_model, 
+        param_distributions=tuning_params, 
+        n_iter=50,
+        scoring='roc_auc',
+        cv=skf,
+        refit=True,
+        verbose=True)
+
+    gs.fit(X_train,y_train)
+
+    #%%
+    # Best score reached: 0.8651849275748571 with params: {'scale_pos_weight': 14, 'num_leaves': 10, 'n_estimators': 1000, 'max_depth': 5, 'learning_rate': 0.01} 
+
+    # %% Extraction and selection of final features
+    impt_features=utils.get_feature_importance(gs,X_train)
+    impt_features.to_csv(f'output/{RANDOM_PREFIX}_impt_features.csv',index=False)
+    final_features= impt_features[impt_features['importance']>0].feature.values
+    X_train=X_train.loc[:, final_features]
+    X_test=X_test.loc[:,final_features]
+   
+    # %% Final Model
+    best_lgb=lgb.LGBMClassifier().set_params(**gs.best_params_)
+    best_lgb.fit(X_train,y_train)
+
+    # %% Model Performance, AUC and PRC curves
+    utils.calculate_model_performance(
+        best_lgb,
+        X_train,
+        X_test,
+        y_train,
+        y_test,
+        RANDOM_PREFIX
+    )
+
+    # %% Create Submission
+    utils.create_submission(best_lgb,RANDOM_PREFIX)
+
+if __name__ == "__main__":
+    main()
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report, confusion_matrix, roc_curve, roc_auc_score, precision_recall_curve, average_precision_score
 
 # Feature Engineering Functions
 def replace_w_sensible_values(df_):
@@ -13,6 +15,7 @@ def replace_w_sensible_values(df_):
     
     # Age can't be 0
     df.loc[df['age'] == 0, 'age'] = 21
+
     return df
 
 def replace_na(df_):
@@ -21,8 +24,12 @@ def replace_na(df_):
     Output: dataframe without na values
     '''
     df=df_.copy()
+
+    # Assume minimum income of 1
     df['MonthlyIncome']=df['MonthlyIncome'].fillna(1.0)
-    df['NumberOfDependents']=df['NumberOfDependents'].fillna(1)
+    
+    # Consider one's self a dependent
+    df['NumberOfDependents']=df['NumberOfDependents']+1
 
     return df
 
@@ -61,29 +68,67 @@ def add_AgeDecade(df_):
     )
     return df
 
-# def add_DebtRatioBin(df_):
-#     '''
-#     Input: dataframe
-#     Output: dataframe with feature binning DebtRatio
-#     '''
-#     df=df_.copy()
-#     df['DebtRatio']=pd.cut(
-#         x=df['DebtRatio'],
-#         bins=[-0.0001,0.33,0.43,1.0],
-#         labels=[0,0.33,1]
-#     ) 
-#     return df
+
+
+def add_boolean_DebtRatio_33(df_):
+    '''
+    Input: dataframe
+    Output: dataframe with 1 added feature
+    Read domain knowledge that Debt Ratio of 0.33 is commonly used threshold
+    '''
+    
+    df=df_.copy()
+    df['DebtRatio33']=df['DebtRatio']>0.33
+    return df
+
+def add_boolean_DebtRatio_43(df_):
+    '''
+    Input: dataframe
+    Output: dataframe with 1 added feature
+    Read domain knowledge that Debt Ratio of 0.43 is commonly used threshold
+    '''
+    
+    df=df_.copy()
+    df['DebtRatio33']=df['DebtRatio']>0.43
+    return df
+
 
 def add_features_per_dependent(df_):
     '''
     Input: dataframe
-    Output: dataframe with additional 4 transformed features
+    Output: dataframe with additional 5 transformed features
     '''
     df=df_.copy()
     df['MonthlyIncomePerDependent']=df['MonthlyIncome']/df['NumberOfDependents']
     df['NumberRealEstateLoansOrLinesPerDependent']=df['NumberRealEstateLoansOrLines']/df['NumberOfDependents']
     df['NumberOfOpenCreditLinesAndLoansPerDependent']=df['NumberOfOpenCreditLinesAndLoans']/df['NumberOfDependents']
     df['NumberOfTime30-59DaysPastDueNotWorsePerDependent']=df['NumberOfTime30-59DaysPastDueNotWorse']/df['NumberOfDependents']
+    df['NumberOfTime60-89DaysPastDueNotWorsePerDependent']=df['NumberOfTime60-89DaysPastDueNotWorse']/df['NumberOfDependents']
+    return df
+
+def add_features_per_creditline(df_):
+    '''
+    Input: dataframe
+    Output: dataframe with additional 4 transformed features
+    '''
+    df=df_.copy()
+    df['MonthlyIncomePerCreditLine']=df['MonthlyIncome']/df['NumberOfOpenCreditLinesAndLoans']
+    df['NumberRealEstateLoansOrLinesPerCreditLine']=df['NumberRealEstateLoansOrLines']/df['NumberOfOpenCreditLinesAndLoans']
+    df['NumberOfTime30-59DaysPastDueNotWorsePerCreditLine']=df['NumberOfTime30-59DaysPastDueNotWorse']/df['NumberOfOpenCreditLinesAndLoans']
+    df['NumberOfTime60-89DaysPastDueNotWorsePerCreditLine']=df['NumberOfTime60-89DaysPastDueNotWorse']/df['NumberOfOpenCreditLinesAndLoans']
+
+    return df
+
+def add_features_per_estate(df_):
+    '''
+    Input: dataframe
+    Output: dataframe with additional 4 transformed features
+    '''
+    df=df_.copy()
+    df['MonthlyIncomePerEstate']=df['MonthlyIncome']/df['NumberRealEstateLoansOrLines']
+    df['NumberRealEstateLoansOrLinesPerEstate']=df['NumberRealEstateLoansOrLines']/df['NumberRealEstateLoansOrLines']
+    df['NumberOfTime30-59DaysPastDueNotWorsePerEstate']=df['NumberOfTime30-59DaysPastDueNotWorse']/df['NumberRealEstateLoansOrLines']
+    df['NumberOfTime60-89DaysPastDueNotWorsePerEstate']=df['NumberOfTime60-89DaysPastDueNotWorse']/df['NumberRealEstateLoansOrLines']
 
     return df
 
@@ -140,9 +185,8 @@ def add_features_distance_from_std(df_):
     df['NumberOfTime60-89DaysPastDueNotWorseMeanDist']=np.std(df['NumberOfTime60-89DaysPastDueNotWorse'])-df['NumberOfTime60-89DaysPastDueNotWorse']
     df['NumberOfDependentsMeanDist']=np.std(df['NumberOfDependents'])-df['NumberOfDependents']
     return df
-    
-# Plotting functions
 
+# Evaluation functions
 def plot_corr_heatmap(df):
     corr=df.corr()
     mask = np.triu(np.ones_like(corr, dtype=np.bool))   
@@ -151,9 +195,6 @@ def plot_corr_heatmap(df):
     chart=sns.heatmap(train.corr(),cmap="PiYG",mask=mask)
     chart.set_xticklabels([])
     return chart
-
-
-# Checking functions
 
 def check_proportion_of_classes(ser):
     '''
@@ -166,9 +207,112 @@ def check_proportion_of_classes(ser):
     print(vc)
     print(f'Proportion of class 1: {vc[1]/vc.sum()}')
 
+
+def get_feature_importance(model,X_train):
+    feat_df=pd.DataFrame(
+        model.best_estimator_.feature_importances_,
+        index=X_train.columns,
+        columns=['importance']
+    ).sort_values('importance',ascending=False)
+
+    feat_df['normalized_importance']=feat_df['importance']/(
+        feat_df['importance'].sum()+np.finfo(float).eps
+    )
+
+    feat_df['cumulative_importance']=feat_df['normalized_importance'].sort_values(ascending=False).cumsum()
+    feat_df.reset_index(inplace=True)
+    feat_df.rename(
+        columns={'index':'feature'},
+        inplace=True
+    )
+
+    return feat_df
+
+def evaluation_metrics(y, y_pred):
+    """
+    Compute score statistics
+    :param y: series, data label
+    :param y_pred: series / ndarray, classifier score
+    :return
+        performance: dict, performance statistics
+        misc: dict, raw data for performance
+    """
+
+    y_pred_cls = (y_pred > 0.5).astype(int)
+    cr = classification_report(y, y_pred_cls, digits=4, output_dict=True)
+    cm = confusion_matrix(y, y_pred_cls)
+
+    fpr, tpr, _ = roc_curve(y, y_pred)
+    auc = roc_auc_score(y, y_pred)
+
+    precision, recall, _ = precision_recall_curve(y, y_pred)
+    ap = average_precision_score(y, y_pred)
+
+    performance = {
+        'AUC': auc, 'AP': ap, 'F1': cr['1']['f1-score'],
+        'PRECISION': cr['1']['precision'], 'RECALL': cr['1']['recall']
+    }
+
+    misc = {
+        'classification_report': cr, 'confusion_matrix': cm,
+        'fpr': fpr, 'precision': precision, 'recall': recall, 'tpr': tpr
+    }
+
+    return performance, misc
+
+def calculate_model_performance(model, X_trainval, X_test, y_trainval, y_test, prefix):
+    """
+    Calculate model performance
+    :param model: model object implementing `predict` and `predict_proba`
+    :param X_trainval: DataFrame, trainval dataset
+    :param X_test: DataFrame, test dataset
+    :param y_trainval: Series, trainval label
+    :param y_test: Series, test label
+    """
+
+    y_trainval_pred = model.predict_proba(X_trainval)[:, 1]
+    y_test_pred = model.predict_proba(X_test)[:, 1]
+
+    pfm_trainval, misc_trainval = evaluation_metrics(y_trainval, y_trainval_pred)
+    pfm_test, misc_test = evaluation_metrics(y_test, y_test_pred)
+
+    # Print confusion matrix
+    print('\nConfusion Matrix TrainVal:')
+    print(misc_trainval['confusion_matrix'])
+    print('\nConfusion Matrix Test:')
+    print(misc_test['confusion_matrix'])
+
+    print('\nClassification Report TrainVal:')
+    print(pd.DataFrame(misc_trainval['classification_report']))
+    print('\nClassification Report Test:')
+    print(pd.DataFrame(misc_test['classification_report']))
+
+    # Plot ROC curve
+    plt.figure(figsize=(8, 6))
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.plot(misc_trainval['fpr'], misc_trainval['tpr'], label=f'TrainVal, AUC = {pfm_trainval["AUC"]:.3f}')
+    plt.plot(misc_test['fpr'], misc_test['tpr'], label=f'Test, AUC = {pfm_test["AUC"]:.3f}')
+    plt.legend(loc='lower right', prop={'size': 10})
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC Curve')
+    plt.show()
+    plt.savefig(f'output/{prefix}_roc.png')
+
+    # Plot PRC curve
+    plt.figure(figsize=(8, 6))
+    plt.plot(misc_trainval['recall'], misc_trainval['precision'], label=f'TrainVal, AP = {pfm_trainval["AP"]:.3f}')
+    plt.plot(misc_test['recall'], misc_test['precision'], label=f'Test, AP = {pfm_test["AP"]:.3f}')
+    plt.legend(loc='upper right', prop={'size': 10})
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('PRC Curve')
+    plt.show()
+    plt.savefig(f'output/{prefix}_prc.png')
+
 # Submission function
 
-def create_submission(model):
+def create_submission(model,prefix):
     kaggle_test=pd.read_csv('./data/cs-test.csv',index_col=0)
     submission=pd.read_csv('./data/sampleEntry.csv',index_col=0)
     X_kaggle=kaggle_test.drop('SeriousDlqin2yrs',axis=1)
@@ -188,7 +332,6 @@ def create_submission(model):
     submission.Probability=y_pred
 
     #Save as csv
-    prefix=str(np.random.randint(1000,9999))
     filepath=f'./output/{prefix}_submission.csv'
     submission.to_csv(filepath)
     print(f'Saved to {filepath}')
